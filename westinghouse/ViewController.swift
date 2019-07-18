909d3277655cdd6adcc27fada2194aaf97f4c7d9
Danila Balabaev <shattenjagger@yandex.ru>
2019-07-18 18:51:33 +0300

    Initial Commit

---
+++ westinghouse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  westinghouse
+//
+//  Created by Danila Balabaev on 18/07/2019.
+//  Copyright © 2019 Danila Balabaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
